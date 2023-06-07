17ba1294f6d1c55effbce21236775d7a89ba18ff
kimseunghyun-kr <incongnito12@gmail.com>
2023-06-07 19:26:09 +0900

    servletmvc update

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListController.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListController.java
@@ -6,15 +6,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
-public class SpringMemberFormControllerV1 {
+public class SpringMemberListController {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("springmvc/v1/members/new-form")
+    @RequestMapping("springmvc/v1/members")
     public ModelAndView process() {
-        return new ModelAndView("new-form");
+        List<Member> members = memberRepository.findAll();
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+        return mv;
     }
+
 }
